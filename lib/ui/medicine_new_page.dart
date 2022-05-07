0442b6c6d6edaacb36ec256580b27f1f51dab859
kyuuki <kyuuki.japan@gmail.com>
2022-05-07 14:44:00 +0900

    Added validation for input medicine name

--- lib/ui/medicine_new_page.dart
+++ lib/ui/medicine_new_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import '../db/mediala_database.dart';
 import '../model/medicine.dart';
@@ -27,6 +26,9 @@ class MedicineNewPage extends StatelessWidget {
             ElevatedButton(
               child: Text("保存する"),
               onPressed: () async{
+                if (!(_formKey.currentState!.validate())) {
+                  return;
+                }
                 await MediaAlaDatabase.instance.create(Medicine(_nameController.text));
                 //Navigator.pop(context, Medicine(_nameController.text, ""));
                 Navigator.pop(context);
@@ -41,21 +43,56 @@ class MedicineNewPage extends StatelessWidget {
           children: [
             Padding(
               padding: EdgeInsets.all(16.0),
-              child: TextField(
-                enabled: true,
-                maxLength: 80,
-                obscureText: false,
-                maxLines:1,
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.medical_services),
-                  hintText: '薬の名前を入力してください',
-                  labelText: 'お薬名',
-                ),
-                controller: _nameController,
-              ),
+              child: InputForm(_nameController),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+// https://docs.flutter.dev/cookbook/forms/validation
+class InputForm extends StatefulWidget {
+  TextEditingController textEditingController;
+
+  InputForm(this.textEditingController);
+
+  @override
+  InputFormState createState() {
+    return InputFormState(textEditingController);
+  }
+}
+
+final _formKey = GlobalKey<FormState>();  // ここでもいい？
+
+class InputFormState extends State<InputForm> {
+  //final _formKey = GlobalKey<FormState>();
+  TextEditingController textEditingController;
+
+  InputFormState(this.textEditingController);
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: TextFormField(
+        enabled: true,
+        maxLength: 80,
+        obscureText: false,
+        maxLines:1,
+        decoration: const InputDecoration(
+          icon: Icon(Icons.medical_services),
+          hintText: '薬の名前を入力してください',
+          labelText: 'お薬名',
+        ),
+        controller: textEditingController,
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return "お薬名を入力してください。";
+          }
+          return null;
+        },
       ),
     );
   }
